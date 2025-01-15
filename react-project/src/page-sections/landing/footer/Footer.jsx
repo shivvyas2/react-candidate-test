1a68d9a1867803e24af717124764b90036ac6b53
Shiv Vyas <sv21670n@pace.edu>
2025-01-14 20:33:33 -0500

    chore(Project) Project Init

---
+++ react-project/src/page-sections/landing/footer/Footer.jsx
@@ -0,0 +1,61 @@
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Container from '@mui/material/Container'; // CUSTOM COMPONENTS
+
+import FlexBox from '@/components/flexbox/FlexBox';
+import { H6, Paragraph } from '@/components/typography'; // STYLED COMPONENTS
+
+import { HeaderTop, TicketWrapper } from './styles';
+export default function Footer() {
+  return <footer>
+      <HeaderTop>
+        <Container maxWidth="xl">
+          <Box pt={{
+          sm: 12,
+          xs: 8
+        }} pb={{
+          sm: 24,
+          xs: 20
+        }}>
+            <H6 mb={4} color="white" fontWeight={700} lineHeight={1.4} fontSize={{
+            sm: 36,
+            xs: 27
+          }}>
+              Streamline your workflow <br /> with Essence
+            </H6>
+
+           
+          </Box>
+
+          <img alt="footer" src="/static/landing/illustration.svg" className="illustration" />
+        </Container>
+      </HeaderTop>
+
+      <Container>
+        <TicketWrapper>
+          <Paragraph fontSize={{
+          sm: 24,
+          xs: 18
+        }} fontWeight={600} mb={3}>
+            Have any questions?
+          </Paragraph>
+
+          <FlexBox justifyContent="center" alignItems="center" gap={2}>
+    
+
+            <Button target="_blank" LinkComponent="a" variant="outlined" href="mailto:support@futeur.ai?subject=Candidate React Query">
+              Send an email
+            </Button>
+          </FlexBox>
+        </TicketWrapper>
+      </Container>
+
+      <Paragraph fontSize={16} textAlign="center" py={6}>
+        Copyright ©{' '}
+        <Box component="a" href="https://www.futeur.com" target="_blank">
+        Futeur AI
+        </Box>
+        . All rights reserved
+      </Paragraph>
+    </footer>;
+}
