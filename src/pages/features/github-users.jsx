17e092b5d52d3ed881b60e150a0c6672b2502724
Siddharth Singh <singhsiddharth0127@gmail.com>
2025-01-24 04:14:27 -0500

    refactor: remove duplicate navigation from GitHub users page

--- src/pages/features/github-users.jsx
+++ src/pages/features/github-users.jsx
@@ -4,8 +4,7 @@ import GitHubUsers from '@/components/GitHubUsers';
 export default function GitHubUsersPage() {
   return (
     <Container maxWidth="lg" sx={{ py: 3 }}>
-      
-      
+      <Typography variant="h4" sx={{ mb: 3 }}>GitHub Users</Typography>
       <GitHubUsers />
     </Container>
   );
